ac3b6e1b726ff6fdf43a6c21069aa9cb40b3431c
Sc1pex <scipex.dev@gmail.com>
2025-09-24 21:58:21 +0300

    networks page changes

--- mnist_web/ui/src/pages/netwokrs.tsx
+++ mnist_web/ui/src/pages/netwokrs.tsx
@@ -1,18 +1,38 @@
 import { createResource, For, Suspense } from "solid-js";
 import { getNetroks } from "../data/temp/networks";
+import ExternalLink from "lucide-solid/icons/external-link";
+import Plus from "lucide-solid/icons/plus";
 
 function Networks() {
   const [data] = createResource(getNetroks);
 
+  const handleCreateNetwork = () => {
+    // TODO: Implement network creation logic
+  };
+
   return (
     <div class="w-2xl mx-auto">
-      <div class="flex flex-col gap-6 mt-8">
+      <div class="flex flex-col gap-6 my-8">
+        {/* Create Network Button */}
+        <div class="flex justify-end">
+          <button class="btn btn-primary" onClick={handleCreateNetwork}>
+            <Plus class="w-4 h-4" />
+            Create Network
+          </button>
+        </div>
+
         <Suspense fallback={<div>Loading...</div>}>
           <For each={data()}>
             {(item) => (
-              <div class="card bg-base-200 shadow-sm">
-                <div class="card-body">
-                  <h2 class="card-title">Network #{item.id}</h2>
+              <div class="card bg-base-200 shadow-sm cursor-pointer hover:shadow-md hover:bg-base-300 transition duration-200">
+                <div class="card-body text-lg">
+                  <h2 class="card-title text-2xl">Network #{item.id}</h2>
+                  <p> Shape: [{item.shape.join(", ")}]</p>
+                  <p> Training iterations: {item.training_iterations}</p>
+                  <p> Cost: {item.cost.toFixed(4)}</p>
+                  <div class="card-actions justify-end">
+                    <ExternalLink />
+                  </div>
                 </div>
               </div>
             )}
