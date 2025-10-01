4f646275df6784fb138f566d9c0d777db07b659d
Sc1pex <scipex.dev@gmail.com>
2025-10-01 22:51:54 +0300

    add httc library

--- mnist_web/server/src/main.cpp
+++ mnist_web/server/src/main.cpp
@@ -1,5 +1,14 @@
-#include <print>
+#include <httc/server.h>
+#include <memory>
+#include "httc/request.h"
+#include "httc/response.h"
+#include "httc/router.h"
 
 int main() {
-    std::println("Hello, world");
+    auto r = std::make_shared<httc::Router>();
+    r->route("/", [](const httc::Request& req, httc::Response& res) {
+        res.set_body("hello there");
+    });
+
+    httc::bind_and_listen("127.0.0.1", 6969, r);
 }
