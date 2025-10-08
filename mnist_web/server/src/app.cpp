aa4c85250899cb559a6fdc67db721a90f8c8ff56
Sc1pex <scipex.dev@gmail.com>
2025-10-08 14:41:59 +0300

    update to new version of httc

--- mnist_web/server/src/app.cpp
+++ mnist_web/server/src/app.cpp
@@ -1,12 +1,25 @@
 #include "app.h"
 #include <httc/server.h>
+#include <asio.hpp>
 #include <memory>
 
 App::App() {
+    using namespace std::chrono_literals;
+
     m_router = std::make_shared<httc::Router>();
-    m_router->route("/", [](const httc::Request& req, httc::Response& res) {
-        res.set_body("hello there");
-    });
+    m_router->route(
+        "/", [](const httc::Request& req, httc::Response& res) -> asio::awaitable<void> {
+            auto ctx = co_await asio::this_coro::executor;
+
+            // Wait for 10 seconds
+            auto timer = asio::steady_timer(ctx, 10s);
+            co_await timer.async_wait(asio::use_awaitable);
+
+            res.set_body("hello there");
+
+            co_return;
+        }
+    );
 }
 
 void App::run() {
