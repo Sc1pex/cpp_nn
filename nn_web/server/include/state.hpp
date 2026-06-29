448595ff9d820fac1110b597bc159d48a0dc671c
Sc1pex <scipex.dev@gmail.com>
2026-06-29 17:21:57 +0300

    add matrices to state

--- nn_web/server/include/state.hpp
+++ nn_web/server/include/state.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
 #include "db.hpp"
+#include "memmat.hpp"
+
+const int TRAIN_SIZE = 60000;
+const int TEST_SIZE = 10000;
+const int IMAGE_SIZE = 28 * 28;
+const int LABEL_SIZE = 10;
 
 struct State {
     State()
-    : db("nn_web.db", "mnist_data/train-images-idx3-ubyte", "mnist_data/train-labels-idx1-ubyte",
-         "mnist_data/t10k-images-idx3-ubyte", "mnist_data/t10k-labels-idx1-ubyte") {
+    : db("nn_web.db"), train_inputs("mnist_data/train_inputs.bin", TRAIN_SIZE, IMAGE_SIZE),
+      train_labels("mnist_data/train_labels.bin", TRAIN_SIZE, LABEL_SIZE),
+      test_inputs("mnist_data/test_inputs.bin", TEST_SIZE, IMAGE_SIZE),
+      test_labels("mnist_data/test_labels.bin", TEST_SIZE, LABEL_SIZE) {
     }
 
     Db db;
+    MemMatrix train_inputs;
+    MemMatrix train_labels;
+    MemMatrix test_inputs;
+    MemMatrix test_labels;
 };
